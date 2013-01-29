8f3d56c366861bf58ba7d6ac23252afcea4f3e05
TAO LIN <t4ol1n@gmail.com>
2013-01-29 23:50:36 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StarAgent::Application.config.secret_token = 'df97de6be0527756978d93871015e17d6a390a8715a662223e9c4b51d2566042c42dbe9de53186af01d7d70f6a0ba82edefdc28198859e415028ce380a9d53e0'
