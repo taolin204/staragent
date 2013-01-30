f8724cae61595eb9481ea9231f2f1ae0595b9d9d
TAO LIN <t4ol1n@gmail.com>
2013-01-30 15:35:48 +0800

    finish devise configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 StarAgent::Application.routes.draw do
+  # get "home/index"
+
   devise_for :users
+
+  root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
